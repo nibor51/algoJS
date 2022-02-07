0d811aff6c7778bb60a0236cb3a6941ebd6a9bec
Nibor51 <jonval.robin@gmail.com>
2022-02-07 12:07:47 +0100

    merge the two function in one function

--- testing.js
+++ testing.js
@@ -26,46 +26,40 @@
 // Après le changement (4,0) : 5,4,2,3,1
 
 
-let string = [5,3,1,2,3,4,5,1,2,1,3,4,0]
+let string = [5,3,1,2,3,4,5,1,2,1,3,4,0];
 
 
 // create function to split array into two arrays, one array with the number of positions and the other with the couple of number of changes
-function splitArray(array) {
-    let position = []
-    let changements = []
+function exercice(array) {
+    let position = [];
+    let changements = [];
     // create a new array with the positions
     for (let i = 2; i < (array[0]+2); i++) {
-        position.push(array[i])
-    }
+        position.push(array[i]);
+    };
     // create a new array with the changes
     for (let i = (array[0]+2); i < array.length; i++) {
-        changements.push(array[i])
-    }
+        changements.push(array[i]);
+    };
     // separate the array of changes into array of array content the position of the first person and the position of the second person
-    let temp = changements
+    let temp = changements;
     for (let i = 0; i < array[1]; i++) {
         if (i === 0) {
-            changements = [[temp[0], temp[1]]]
+            changements = [[temp[0], temp[1]]];
         } else {
-            changements.push([temp[i*2], temp[i*2+1]])
-        }
-    }
+            changements.push([temp[i*2], temp[i*2+1]]);
+        };
+    };
 
-    return [position, changements]
-}
-
-// create a function to return all new positions of the position array after the changes of the changements array
-function newPositions( position, changements) {
-    let newPositions = position
+    let newPositions = position;
     for (let i = 0; i < changements.length; i++) {
-        let temp = newPositions[changements[i][0]]
-        newPositions[changements[i][0]] = newPositions[changements[i][1]]
-        newPositions[changements[i][1]] = temp
-        console.log(newPositions)
-    }
-    return newPositions
+        let temp = newPositions[changements[i][0]];
+        newPositions[changements[i][0]] = newPositions[changements[i][1]];
+        newPositions[changements[i][1]] = temp;
+        console.log(newPositions);
+    };
+
+    return `Donnée en entrée : ${string}\n${changements.length} changement(s) effectué(s) !\nDonnée en sortie : ${newPositions}`;
 }
 
-var split = splitArray(string)
-console.log(split);
-console.log(newPositions(split[0], split[1]));
+console.log(exercice(string));
